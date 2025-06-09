5ac3b617d154cf58a4b21273fd8f2c77bcec87fb
Steve Braich <sbraich@pdx.edu>
2025-06-09 06:26:48 -0700

    added app.razor to Web.Client

--- DotNetMaui.Web.Client/Program.cs
+++ DotNetMaui.Web.Client/Program.cs
@@ -1,10 +1,14 @@
 using DotNetMaui.Shared.Services;
+using DotNetMaui.Web.Client.Components; // We will create this component next
 using DotNetMaui.Web.Client.Services;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-// Add device-specific services used by the DotNetMaui.Shared project
+// Add this line to tell Blazor where the app starts
+builder.RootComponents.Add<App>("#app");
+
+// Add this line to register the device-specific service
 builder.Services.AddSingleton<IFormFactor, FormFactor>();
 
 await builder.Build().RunAsync();
